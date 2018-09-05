09d968f470e92e4cc40ca80fcf571e256c41a6a5
MMPark <parkmee@gmail.com>
2018-09-05 00:41:43 -0400

    wip

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-// define array of words for hangman game
 var words = [
     "lion", 
     "leopard", 
@@ -22,60 +21,52 @@ var words = [
     "vulture",
 ];
 
-// define variables to track score, remaining guesses, and word-in-play
-var win = 0;
-var loss = 0;
-var guess = 9;
-var wordtoGuess;
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var currentWord = "";
+var gameStart = true;
+var lettersGuessed = [];
 
-// define variables to hold references to html where text will be printed
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeft = document.getElementById("guesses-left");
-var lettersGuessed = document.getElementById("letters-guessed");
-var letterBlank = document.getElementById("word-blank");
+var wordBlank = document.getElementById("word-blank");
+var errorMsg = document.getElementById("error-msg");
 
-// define function: on key up, computer randomly selects word from array
+var lettersInWord = [];
 function getWord() {
-    wordtoGuess = words[Math.floor(Math.random() * words.length)];
-    console.log(wordtoGuess);
+    currentWord = words[Math.floor(Math.random() * words.length)];
+    console.log(currentWord);
+    console.log(currentWord.length);
+    for (var i = 0; i < currentWord.length; i ++) {
+        lettersInWord.push("_");
+    };
+};
 
-    // parse letters into divs   
-    for (var i = 0; i < wordtoGuess.length; i++) {
-        var letterBlank = document.createElement("div");
-        letterBlank.textContent = wordtoGuess.charAt(i);
+document.onkeyup = function(event) {
 
-        // concatenate divs on screen
-        letterBlank.append(letterBlank);      
-        
-        // hide letters
+    var userGuess = event.key;
+    console.log(userGuess);
+    for (var j = 0; j < currentWord.length; j++) {
+        if (userGuess === currentWord.charAt[j]) {
+            lettersInWord[j] = userGuess;
+            wordBlank.textContent = lettersInWord.join(" ");
+            console.log(lettersInWord);
+        }
     }
 
+if (gameStart === true) {
+        lettersInWord.length = 0;
+        getWord();
+        wordBlank.textContent = lettersInWord.join(" ","_");
+        gameStart = false;
+    };
+
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesLeft.textContent = "Guesses Left: " + guesses;
+    //guessesLeft.append = userGuess;
+    
 }
-
-// on key up, user guesses letter
-
-    // define variable to record guess
-
-    // reject if guess is not a letter or is a repeated letter
-
-    // otherwise, check if guess equals one or more letters of the word
-
-        // if yes, fill in letter(s)
-
-            // if all letters filled in, increment win count
-
-            // play corresponding sound/picture
-
-            // reset game
-
-        // if no, decrement number of guesses remaining
-
-            // print letters guessed
-
-            // if guesses total 0, increment number of losses and reset game
-
-    // print current score and guesses remaining
-
-
 
